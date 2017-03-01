a3b03b98c344bea27fbb50858e65ce1a5657c802
JodieGarden <jodie.garden@gmail.com>
2017-03-01 10:26:43 +0000

    added extra activity

--- app/src/main/java/com/codeclan/example/calculator/Main3Activity.java
+++ app/src/main/java/com/codeclan/example/calculator/Main3Activity.java
@@ -2,12 +2,16 @@ package com.codeclan.example.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main3Activity extends AppCompatActivity {
+
+    TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        resultText = (TextView) findViewById(R.id.resultText);
     }
 }
--- app/src/main/java/com/codeclan/example/calculator/MainActivity.java
+++ app/src/main/java/com/codeclan/example/calculator/MainActivity.java
@@ -1,13 +1,57 @@
 package com.codeclan.example.calculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText num1;
+    EditText num2;
+    Button addBtn;
+    Button subBtn;
+    Button multBtn;
+    Button divBtn;
+    Calculator calc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        num1 = (EditText) findViewById(R.id.num1);
+        num2 = (EditText) findViewById(R.id.num2);
+        addBtn = (Button) findViewById(R.id.add);
+        subBtn = (Button) findViewById(R.id.subtract);
+        multBtn = (Button) findViewById(R.id.multiply);
+        divBtn = (Button) findViewById(R.id.divide);
+        calc = new Calculator();
+    }
+
+    public void onCalcButtonClick(View view){
+        int number1 = Integer.parseInt(num1.getText().toString());
+        int number2 = Integer.parseInt(num2.getText().toString());
+        int result = 0;
+        switch(view.getId()){
+            case R.id.add:
+                result = calc.add(number1, number2);
+                break;
+            case R.id.subtract:
+                result = calc.subtract(number1, number2);
+                break;
+            case R.id.multiply:
+                result = calc.multiply(number1, number2);
+                break;
+            case R.id.divide:
+                result = calc.divide(number1, number2);
+                break;
+        }
+
+        Intent intent = new Intent(MainActivity.this, Main3Activity.class);
+        intent.putExtra("answer", result);
+        startActivity(intent);
     }
 }
